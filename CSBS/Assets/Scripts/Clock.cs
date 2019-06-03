eb4f230917aec6e93b0dca9a6f5d34edda3f504c
janiceng445 <janiceng@ufl.edu>
2019-06-03 12:02:33 -0400

    Resuming

---
+++ CSBS/Assets/Scripts/Clock.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clock : MonoBehaviour
+{
+    public Text text;
+    public int CurrHour;
+    
+    void Update()
+    {
+        CurrHour = GameData.hour;
+        text.text = "Day " + CurrHour;
+    }
+}
